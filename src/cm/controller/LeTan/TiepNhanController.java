d56254e6fb34aec66aa537e171aa2c2abb57bc1b
vinh <vinh@vinh-K55VD>
2015-11-08 10:40:20 +0700

    update hien thong tin benh nhan

--- src/cm/controller/LeTan/TiepNhanController.java
+++ src/cm/controller/LeTan/TiepNhanController.java
@@ -6,13 +6,22 @@
 package cm.controller.LeTan;
 
 import cm.ConnectToDatabase;
+import cm.model.BenhNhan;
 import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  *
@@ -33,10 +42,77 @@ public class TiepNhanController implements Initializable{
     private Label lblDiaChi;
     @FXML
     private Label lblPhone;
+    @FXML
+    private TableView<BenhNhan> BenhNhanTable;
+    @FXML
+    private TableColumn MaColumn;
+    @FXML
+    private TableColumn TenColumn;
+    @FXML
+    private TableColumn NgaySinhColumn;
+    @FXML
+    private TableColumn GioiTinhColumn;
+    @FXML
+    private TableColumn PhoneColumn;
+    @FXML
+    private TableColumn ThoiGianColumn;
+    @FXML
+    private TableColumn TrangThaiColumn;
+    
+    private ObservableList<BenhNhan> BenhNhanData = FXCollections.observableArrayList();
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        con = new ConnectToDatabase();
+        try {
+            con = new ConnectToDatabase();
+            addBenhNhanData();
+            initTable();
+        } catch (SQLException ex) {
+            Logger.getLogger(TiepNhanController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
+    public void addBenhNhanData() throws SQLException{
+        String sql = "SELECT * FROM Benh_Nhan";
+        rs = con.getRS(sql);
+        while(rs.next()){
+            BenhNhan benhnhan = new BenhNhan();
+            benhnhan.setMa(rs.getInt("Ma_Benh_Nhan"));
+            benhnhan.setHoTen(rs.getString("Ho_Ten"));
+            benhnhan.setNgaySinh(rs.getString("Ngay_Sinh"));
+            benhnhan.setGioiTinh(rs.getString("Gioi_Tinh"));
+            benhnhan.setPhone(rs.getString("SDT_BN"));
+            benhnhan.setTrangThai(rs.getString("Trang_Thai"));
+            benhnhan.setDiaChi(rs.getString("Dia_chi"));
+            BenhNhanData.add(benhnhan);
+        }
+        rs.close();
+    }
+    public void initTable(){
+        MaColumn.setCellValueFactory(new PropertyValueFactory<>("Ma"));
+        TenColumn.setCellValueFactory(new PropertyValueFactory<>("HoTen"));
+        NgaySinhColumn.setCellValueFactory(new PropertyValueFactory<>("NgaySinh"));
+        GioiTinhColumn.setCellValueFactory(new PropertyValueFactory<>("GioiTinh"));
+        PhoneColumn.setCellValueFactory(new PropertyValueFactory<>("Phone"));
+        TrangThaiColumn.setCellValueFactory(new PropertyValueFactory<>("TrangThai"));
+        BenhNhanTable.setItems(BenhNhanData);
+        BenhNhanTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> showUserDetails(newValue));
+    }
+
+    private void showUserDetails(BenhNhan benhnhan) {
+        if(benhnhan != null){
+            lblTen.setText(benhnhan.getHoTen());
+            lblNgaySinh.setText(benhnhan.getNgaySinh());
+            lblGioiTinh.setText(benhnhan.getGioiTinh());
+            lblPhone.setText(benhnhan.getPhone());
+            lblDiaChi.setText(benhnhan.getDiaChi());
+        }
+        else{
+            lblTen.setText("");
+            lblNgaySinh.setText("");
+            lblGioiTinh.setText("");
+            lblPhone.setText("");
+            lblDiaChi.setText("");
+        }
+    }
+   
 }
