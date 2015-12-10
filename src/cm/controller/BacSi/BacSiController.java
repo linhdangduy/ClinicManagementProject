b914785fe05cbd2e644528b24fbe18f94a658b8b
Mai Hoàng Đức <hoangduc1248@gmail.com>
2015-12-10 10:16:25 +0700

    Update BacSiController.java

--- src/cm/controller/BacSi/BacSiController.java
+++ src/cm/controller/BacSi/BacSiController.java
@@ -30,80 +30,87 @@ import javafx.scene.layout.StackPane;
  * @author linhsan
  */
 public class BacSiController implements Initializable {
-    
+
     @FXML
     private StackPane StackPane;
     @FXML
     private Label lblDangXuat;
     @FXML
     private Label lblTenBacsi;
-    
+
     private HashMap<String, HBox> screens = new HashMap<>();
-    
+
     private void loadPane(String paneName, String resource) {
         try {
             FXMLLoader load = new FXMLLoader(getClass().getResource(resource));
             HBox pane = load.load();
-            PaneInterface paneController = (PaneInterface)load.getController();
+            PaneInterface paneController = (PaneInterface) load.getController();
             paneController.setScreenParent(this);
-            screens.put(paneName, pane);           
+            screens.put(paneName, pane);
         } catch (IOException ex) {
             Logger.getLogger(BacSiController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    
+
     public void setPane(String pane) {
         if (!StackPane.getChildren().isEmpty()) {
             StackPane.getChildren().remove(0);
         }
         StackPane.getChildren().add(0, screens.get(pane));
     }
-    
+
     @FXML
-    private void handleBtnTiepNhan(ActionEvent event) {        
+    private void handleBtnTiepNhan(ActionEvent event) {
         setPane("tiepnhan");
     }
-    
+
     @FXML
     private void handleBtnThuoc(ActionEvent event) {
         setPane("thuoc");
         ControllerMediator.getInstance().getThuocCtrl().setPaneThemThuoc(false);
     }
-    
+
     @FXML
     private void handleBtnDichVu(ActionEvent event) {
         setPane("dichvu");
         ControllerMediator.getInstance().getDichVuCtrl().setPaneThemDichVu(false);
     }
-    
+
     @FXML
     private void mouseEnteredLblDangXuat(MouseEvent event) {
-        
+
     }
+
     @FXML
     private void mouseExitedLblDangXuat(MouseEvent event) {
-        
+
     }
+
     @FXML
     private void mouseClickedLblDangXuat(MouseEvent event) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("/cm/view/DangNhap/DangNhap.fxml"));
             Scene scene = new Scene(root);
             ClinicManager.getStage().setScene(scene);
-           
+
             ConnectToServer con = new ConnectToServer();
             String query = "UPDATE Tai_Khoan SET Trang_Thai = 'Nghỉ' WHERE Ten_Dang_Nhap =  '"
-                                + DangNhapController.getTenDangNhap() +"'";
+                    + DangNhapController.getTenDangNhap() + "'";
             con.sendToServer(query);
             con.sendToServer("done");
         } catch (IOException ex) {
             Logger.getLogger(BacSiController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        ClinicManager.getStage().setOnCloseRequest(e -> {
+            String sql = "UPDATE Tai_Khoan set Trang_Thai='Nghỉ' where Ten_Dang_Nhap = '" + DangNhapController.getTenDangNhap() + "';";
+            ConnectToServer con = new ConnectToServer();
+            con.sendToServer(sql);
+            con.sendToServer("done");
+        });
         loadPane("tiepnhan", "/cm/view/BacSi/TiepNhan.fxml");
         loadPane("thuoc", "/cm/view/BacSi/Thuoc.fxml");
         loadPane("dichvu", "/cm/view/BacSi/DichVu.fxml");
@@ -111,5 +118,4 @@ public class BacSiController implements Initializable {
         lblTenBacsi.setText(DangNhapController.getEmployeeName());
     }
 
-    
 }
